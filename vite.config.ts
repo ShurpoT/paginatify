b3e29d82e00528fb71d3b8be6cff0f1734ba04cd
ShurpoT <timashurpo@gmail.com>
2025-07-25 17:54:00 +0300

    project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,53 @@ export default defineConfig({
         open: true,
     },
 
-    plugins: [react()],
+    build: {
+        sourcemap: true,
+        minify: false,
+
+        lib: {
+            entry: "src/Components/paginatify/paginatify.tsx",
+            name: "paginatify",
+            fileName: "paginatify",
+        },
+
+        rollupOptions: {
+            external: (id) => {
+                return ["react", "react-dom", "react/jsx-runtime"].includes(id);
+            },
+
+            output: [
+                {
+                    format: "es",
+                    dir: "paginatify",
+                    entryFileNames: "paginatify.js",
+                    globals: {
+                        react: "React",
+                        "react-dom": "ReactDOM",
+                    },
+                },
+                {
+                    format: "cjs",
+                    dir: "paginatify",
+                    entryFileNames: "paginatify.cjs.js",
+                    globals: {
+                        react: "React",
+                        "react-dom": "ReactDOM",
+                    },
+                },
+            ],
+        },
+    },
+
+    plugins: [
+        react(),
+        dts({
+            include: ["src/Components/paginatify/**/*"],
+            exclude: ["src/Components/paginatify/**/*.test.tsx"],
+
+            outDir: "paginatify",
+            staticImport: true,
+            cleanVueFileName: true,
+        }),
+    ],
 });
