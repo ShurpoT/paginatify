71fa3492756f350c5ab342283d13a7ceaca8d2cc
ShurpoT <timashurpo@gmail.com>
2025-07-26 23:33:38 +0300

    create pagination component

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Paginatify } from "./Components/paginatify/paginatify";
+
+import "./App.css";
+
+type Data = {
+    albumId: number;
+    id: number;
+    title: number;
+    url: number;
+    thumbnailUrl: number;
+};
+
+const perPage = 4;
+
+function App() {
+    const [items, setItems] = useState<Data[]>([]);
+    const [paginatifyLength, setPaginatifyLength] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        // JSONPlaceholder doesn't return the total number of items.
+        // Therefore, we use the second FETCH to calculate the pagination length.
+        // If you use filters, just include them in both requests.
+
+        //Method for fetching data for the current page only.
+        fetch(`https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=${perPage}`)
+            .then((res) => res.json())
+            .then((data) => setItems(data));
+
+        // Method for fetching all data and calculating the pagination length.
+        fetch(`https://jsonplaceholder.typicode.com/photos`)
+            .then((res) => res.json())
+            .then((data) => setPaginatifyLength(Math.ceil(data.length / perPage)))
+            .finally(() => setIsLoading(false));
+    }, [currentPage]);
+
+    const handlePageClick = (i: number) => {
+        setCurrentPage(i);
+    };
+
+    return (
+        <>
+            <Paginatify
+                className="paginatify"
+                paginatifySize={paginatifyLength}
+                paginatifyStep={3}
+                onPageChange={handlePageClick}
+            />
+
+            {/* ==== YOUR CONTENT ==== */}
+            {isLoading ? (
+                <p className="text">Loading...</p>
+            ) : (
+                <ul className="list">
+                    {items.map((item, i) => (
+                        <li key={i} className="item">
+                            {item.title}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/paginatify/paginatify.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+// import "./paginatify.css";
+
+const throttle = <T extends (...args: any[]) => any>(func: T, ms: number): ((...args: Parameters<T>) => void) => {
+    let lastCall = 0;
+    return function (...args: Parameters<T>) {
+        const now = Date.now();
+        if (now - lastCall >= ms) {
+            lastCall = now;
+            return func(...args);
+        }
+    };
+};
+
+interface IPaginatify {
+    className?: string;
+
+    paginatifySize: number;
+    paginatifyStep?: number;
+    onPageChange: (i: number) => void;
+}
+
+export const Paginatify = ({ className = "paginatify", paginatifySize, paginatifyStep = 2, onPageChange }: IPaginatify) => {
+    const [currentPage, setCurrentPage] = useState(1),
+        pageChange = throttle((pageNumber) => {
+            if (pageNumber < 1 || pageNumber > paginatifySize) return;
+            onPageChange(pageNumber);
+            setCurrentPage(pageNumber);
+        }, 1000),
+        createBreak = (key: string) => (
+            <li key={key} className={`${className}__break`}>
+                ...
+            </li>
+        ),
+        first = () => [...add(1, 2), createBreak("fb")],
+        last = () => [createBreak("lb"), ...add(paginatifySize, paginatifySize + 1)],
+        add = (start: number, end: number) =>
+            Array.from({ length: end - start }, (_, i) => {
+                const numb = start + i;
+                return (
+                    <li
+                        key={numb}
+                        className={`${className}__page${currentPage === numb ? ` ${className}__page--active` : ""}`}
+                        onClick={() => pageChange(numb)}>
+                        {numb}
+                    </li>
+                );
+            });
+
+    if (!paginatifySize) return;
+
+    return (
+        <ul className={className}>
+            <li
+                key="p"
+                className={`${className}__arrow--prev${currentPage === 1 ? ` ${className}__arrow--disabled` : ""}`}
+                onClick={() => pageChange(currentPage - 1)}>
+                ◄
+            </li>
+
+            {paginatifySize < paginatifyStep * 2 + 6
+                ? add(1, paginatifySize + 1)
+                : currentPage < paginatifyStep * 2 + 2
+                ? [...add(1, paginatifyStep * 2 + 4), last()]
+                : currentPage > paginatifySize - paginatifyStep * 2 - 1
+                ? [first(), ...add(paginatifySize - paginatifyStep * 2 - 2, paginatifySize + 1)]
+                : [first(), ...add(currentPage - paginatifyStep, currentPage + paginatifyStep + 1), last()]}
+
+            <li key="CT" className={`${className}__counter`}>
+                {currentPage} / {paginatifySize}
+            </li>
+
+            <li
+                key="n"
+                className={`${className}__arrow--next${currentPage === paginatifySize ? ` ${className}__arrow--disabled` : ""}`}
+                onClick={() => pageChange(currentPage + 1)}>
+                ►
+            </li>
+        </ul>
+    );
+};
